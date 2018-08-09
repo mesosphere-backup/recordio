0c31b1b231f7dba43a08d41800447f3cc3cfddb5
Fabs <fnascimento@mesosphere.com>
2018-08-09 14:58:40 +0200

    ran prettier

--- src/read.js
+++ src/read.js
@@ -5,18 +5,18 @@ var copychars = require("@dcos/copychars").default;
 var RECORD_PATTERN = /^\d+\n.+/;
 
 function countExtraCodepoints(string, start, end) {
-    let correctedLength = end - start;
-    let length = end - start;
-    for (let i = end - start; i >= start; i--) {
-        const codePoint = string.charCodeAt(i);
+  let correctedLength = end - start;
+  let length = end - start;
+  for (let i = end - start; i >= start; i--) {
+    const codePoint = string.charCodeAt(i);
 
-        if (codePoint > 0x7f && codePoint <= 0x7ff) correctedLength++;
-        else if (codePoint > 0x7ff && codePoint <= 0xffff) correctedLength += 2;
+    if (codePoint > 0x7f && codePoint <= 0x7ff) correctedLength++;
+    else if (codePoint > 0x7ff && codePoint <= 0xffff) correctedLength += 2;
 
-        if (codePoint >= 0xDC00 && codePoint <= 0xDFFF) i--;
-    }
+    if (codePoint >= 0xdc00 && codePoint <= 0xdfff) i--;
+  }
 
-    return correctedLength - length;
+  return correctedLength - length;
 }
 
 module.exports = function read(input) {
@@ -40,7 +40,11 @@ module.exports = function read(input) {
     //Solves an issue with string.length where a character composed by multiple
     //codePoints, out of the ASCII range, (e.g. ß) makes us miss the size of the
     //message.
-    byteCorrection = countExtraCodepoints(rest, recordStartPosition, recordEndPosition)
+    byteCorrection = countExtraCodepoints(
+      rest,
+      recordStartPosition,
+      recordEndPosition
+    );
     recordEndPosition -= byteCorrection;
     recordLength -= byteCorrection;
 
